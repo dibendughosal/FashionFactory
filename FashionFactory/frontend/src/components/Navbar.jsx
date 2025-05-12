5fd1793cb4d789cf6fb61b7e70467d8dd1c586d3
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-05-12 11:28:29 +0530

    Fixed

--- FashionFactory/frontend/src/components/Navbar.jsx
+++ FashionFactory/frontend/src/components/Navbar.jsx
@@ -9,18 +9,18 @@ const Navbar = () => {
 	const { cart } = useCartStore();
 
 	return (
-		<header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
+		<header className='fixed top-0 left-0 w-full bg-[#ffffff] bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
 			<div className='container mx-auto px-4 py-3'>
 				<div className='flex flex-wrap justify-between items-center'>
-					<Link to='/' className='text-2xl font-bold text-emerald-400 items-center space-x-2 flex'>
-						FashionFactory <span>by Dibendu</span>
+					<Link to='/' className='text-2xl font-bold text-[#333333] items-center space-x-2 flex'>
+						<h1>FashionFactory</h1><span className="text-red-400"><sup>by</sup>Dibendu</span>
 					</Link>
 
 					<nav className='flex flex-wrap items-center gap-4'>
 						<Link
 							to={"/"}
-							className='text-gray-300 hover:text-emerald-400 transition duration-300
-					 ease-in-out'
+							className='text-gray-900 hover:text-emerald-400 transition duration-300
+					 ease-in-out font-bold'
 						>
 							Home
 						</Link>
